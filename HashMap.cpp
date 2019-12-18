8819e078c7e40835eec7f346698364d3bfdcb7b3
shoamco <shoamc98@gmail.com>
2019-12-17 23:17:18 -0800

    add defaultHashFunc

--- HashMap.cpp
+++ HashMap.cpp
@@ -1,4 +1,20 @@
-//
-// Created by shoam on 12/17/19.
-//
 
+
+#include "HashMap.h"
+size_t defaultHashFunc(const string& key, size_t hashSize)
+{
+    /**
+     default Hash function=
+     (size keys )* (sum all keys) *( (Multiplication of all even keys)+(Multiplication of all odd keys))%hashSize
+     * */
+    size_t size = key.size(), sumAll = 0, evenMult = 1, oddMult = 1;
+    for(size_t i = 0; i < size; ++i)
+    {
+        sumAll += key[i];
+        if(i % 2 == 0)//if even
+            evenMult *= key[i];
+        else
+            oddMult *= key[i];
+    }
+    return (size * sumAll * (evenMult + oddMult)) % hashSize;
+}
