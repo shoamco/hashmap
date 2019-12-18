3b940c5e4e2b790172448c8f5d0629d5f83fbfcc
shoamco <shoamc98@gmail.com>
2019-12-18 07:31:34 -0800

    update main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include "TestHashMap.h"
 int main() {
-
+test_create_hash_map();
+    test_insert();
+//    test_list();
+    test_getNumOfBucketsInUse();
+     test_getter();
+    test_remove();
+    test_operator_square();
     return 0;
 }
